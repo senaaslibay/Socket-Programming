446207a8dc3d58cd1c9047f9dc6d488ebfb43592
Sena Aslıbay <senaaslibay@gmail.com>
2019-08-13 17:02:34 +0300

    This is the first project i have ever done about socket programming.

---
+++ SocketProgramlama/Client/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
+using System.Collections;
+
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        //Burda server da tanımladıklarımızdan farklı olarak TcpClient sınıfı ile serverdan gelen bilgileri alıyoruz
+        public TcpClient istemci;
+        private NetworkStream AgAkimi;
+        private StreamReader AkimOkuyucu;
+        private StreamWriter AkimYazici;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                istemci = new TcpClient("localhost", 1234);
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Bağlanamadı..");
+                return;
+            }
+
+            //Server programında yaptıklarımızı burda da yapıyoruz.
+            AgAkimi = istemci.GetStream();
+            AkimOkuyucu = new StreamReader(AgAkimi);
+            AkimYazici = new StreamWriter(AgAkimi);
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Kullanıcı butona her tıkladığında textbox'ta yazı yoksa uyarı veriyoruz
+            //Sonra AkimYazici vasıtası ile AgAkımına veriyi gönderip sunucudan gelen 
+            //cevabı AkimOkuyucu ile alıp Mesaj la kullanıcıya gösteriyoruz 
+            //Tabi olası hatalara karşı, Sunucuya bağlanmada hata oluştu mesajı veriyoruz.
+
+            try
+            {
+                if (textBox1.Text=="")
+                {
+                    MessageBox.Show("Lütfen bir yazı giriniz", "Uyarı");
+                    textBox1.Focus();
+                    return;
+                }
+
+                string yazi;
+                AkimYazici.WriteLine(textBox1.Text);
+                AkimYazici.Flush();
+                yazi = AkimOkuyucu.ReadLine();
+                MessageBox.Show(yazi, "Sunucudan mesaj var.");
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Sunucuya bağlanmada hata var.");
+
+                throw;
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            try
+            {
+                AkimYazici.Close();
+                AkimOkuyucu.Close();
+                AgAkimi.Close();
+            }
+
+            catch
+            {
+                MessageBox.Show("Düzgün kapatilamiyor");
+            }
+        }
+
+        //Ve bütün oluşturduğumuz nesneleri form kapatıldığında kapatıyoruz.
+    }
+}
---
+++ server/server/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Net.Sockets;
+using System.IO;
+
+
+namespace Server
+{
+    public class Server
+    {
+        public static void Main()
+        {
+            //Bilgi alisverisi için bilgi almak istedigimiz port numarasini TcpListener sinifi ile gerçeklestiriyoruz
+            TcpListener TcpDinleyicisi = new TcpListener(1234);
+            TcpDinleyicisi.Start();
+
+            Console.WriteLine("Sunucu Başlatıldı...");
+
+            //Soket baglantimizi yapiyoruz.Bunu TcpListener sinifinin AcceptSocket metodu ile yapıyoruz
+            Socket IstemciSoketi = TcpDinleyicisi.AcceptSocket();
+
+            // Baglantının olup olmadığını kontrol ediyoruz
+            if (!IstemciSoketi.Connected)
+            {
+                Console.WriteLine("Sunucu Başlatılamıyor!");
+            }
+            else
+            {
+                //Sonsuz döngü sayesinde AgAkimini sürekli okuyoruz
+                while (true)
+                {
+                    Console.WriteLine("İstemci bağlantısı sağlandı.");
+
+                    //IstemciSoketi verilerini NetworkStream sinifi türünden nesneye aktariyoruz.
+                    NetworkStream AgAkimi = new NetworkStream(IstemciSoketi);
+
+                    //Soketteki bilgilerle islem yapabilmek için StreamReader ve StreamWriter siniflarini kullaniyoruz
+                    StreamWriter AkimYazici = new StreamWriter(AgAkimi);
+                    StreamReader AkimOkuyucu = new StreamReader(AgAkimi);
+
+                    //StreamReader ile String veri tipine aktarma islemi önceden bir hata olursa bunu handle etmek gerek
+                    try
+                    {
+                        string IstemciString = AkimOkuyucu.ReadLine();
+                        Console.WriteLine("Gelen bilgi:" + IstemciString);
+
+                        //Istemciden gelen bilginin uzunlugu hesaplaniyor
+                        int uzunluk = IstemciString.Length;
+
+                        //AgAkimina, AkimYazını ile IstemciString inin uzunluğunu yazıyoruz 
+                        AkimYazici.WriteLine(uzunluk.ToString());
+
+                        AkimYazici.Flush();
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Sunucu kapatılıyor..");
+                        return;
+                    }
+
+                }
+
+            }
+            IstemciSoketi.Close();
+            Console.WriteLine("Sunucu Kapatiliyor...");
+        }
+
+    }
+}
